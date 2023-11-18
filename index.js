ae9e22b7b2770458b0c74f435a02d7a1a1235cee
Jean Leite <jgusleite@gmail.com>
2023-11-18 11:31:53 -0300

    Add rainbow mode

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".grid-container");
 const clearBtn = document.querySelector(".clear");
 const gridSizeBtn = document.querySelector(".grid-size");
+const rainbowModeBtn = document.querySelector(".rainbow-mode");
 
 let grid = null;
 let gridWidth = 8;
 let gridHeight = 8;
+let rainbowMode = false;
 
 clearBtn.addEventListener("click", (e) => {
   clearGrid();
@@ -12,6 +14,27 @@ clearBtn.addEventListener("click", (e) => {
 
 gridSizeBtn.addEventListener("click", (e) => {
   setGridSize();
+});
+
+rainbowModeBtn.addEventListener("click", (e) => {
+  rainbowMode = !rainbowMode;
+
+  if (rainbowMode) {
+    rainbowModeBtn.style.background = `linear-gradient( 90deg,
+        rgba(255, 0, 0, 1) 0%,
+        rgba(255, 154, 0, 1) 10%,
+        rgba(208, 222, 33, 1) 20%,
+        rgba(79, 220, 74, 1) 30%,
+        rgba(63, 218, 216, 1) 40%,
+        rgba(47, 201, 226, 1) 50%,
+        rgba(28, 127, 238, 1) 60%,
+        rgba(95, 21, 242, 1) 70%,
+        rgba(186, 12, 248, 1) 80%,
+        rgba(251, 7, 217, 1) 90%,
+        rgba(255, 0, 0, 1) 100%)`;
+  } else {
+    rainbowModeBtn.style.background = "#fff";
+  }
 });
 
 function createGrid() {
@@ -31,15 +54,23 @@ function updateGrid() {
   grid = document.querySelectorAll(".grid-container > div");
   grid.forEach((item) => {
     item.addEventListener("mouseover", (e) => {
-      item.style.backgroundColor = "black";
+      if (!rainbowMode) item.style.backgroundColor = "black";
+      else {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        let color = `rgb(${r}, ${g}, ${b})`;
+        item.style.backgroundColor = color;
+      }
     });
   });
 }
 
 function setGridSize() {
-  let size = +prompt("Size [8, 64]: ");
+  let size = +prompt("Size [1, 64]: ");
+  if (size == 0) size = 1;
 
-  if (size % 8 !== 0 || size > 64) {
+  if (size > 64) {
     return alert("Invalid Size!");
   }
 
